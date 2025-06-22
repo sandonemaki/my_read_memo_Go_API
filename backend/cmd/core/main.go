7c3aede474d2d57deaedb5d9b533ba05f8e862dd
sandonemaki <kamuramaki@gmail.com>
2025-06-22 22:15:37 +0900

    mainでgoのサーバーを立ち上げ

---
+++ backend/cmd/core/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Printf("Hello, World!\n")
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	// サーバー起動
+	log.Println("サーバーを起動します。ポート：8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("サーバー起動に失敗しました: %v", err)
+	} else {
+		log.Println("サーバーが正常に起動しました。")
+	}
+}
