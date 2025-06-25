8cdd8d2f758e5834d7a65637310fb821cdb41e07
sandonemaki <kamuramaki@gmail.com>
2025-06-25 22:41:07 +0900

    ジェネリック型を直接使用

--- backend/core/domain/model/user.go
+++ backend/core/domain/model/user.go
@@ -3,32 +3,23 @@ package model
 import (
 	"database/sql"
 	"time"
-
-	"github.com/sandonemaki/my_read_memo_Go_API/backend/pkg/dbmodels"
-	"github.com/volatiletech/null/v8"
 )
 
-type User dbmodels.User
+type User struct {
+	Ulid      string
+	Nickname  string
+	DeletedAt sql.Null[time.Time]
+}
 
 func NewUser(
 	ulid string,
 	nickname string,
-	deletedAt null.Time,
+	deletedAt sql.Null[time.Time],
 ) *User {
-	var sqlDeletedAt sql.Null[time.Time]
-	if deletedAt.Valid {
-		sqlDeletedAt = sql.Null[time.Time]{
-			V:     deletedAt.Time,
-			Valid: true,
-		}
-	} else {
-		sqlDeletedAt = sql.Null[time.Time]{
-			V: time.Time{},
-		}
-	}
+
 	return &User{
 		Ulid:      ulid,
 		Nickname:  nickname,
-		DeletedAt: sqlDeletedAt,
+		DeletedAt: deletedAt,
 	}
 }
