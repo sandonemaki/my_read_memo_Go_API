c553e18783899f41acb52a34e75ff716f29a3e15
sandonemaki <kamuramaki@gmail.com>
2025-06-26 08:23:02 +0900

    user model修正

--- backend/core/domain/model/user.go
+++ backend/core/domain/model/user.go
@@ -3,13 +3,11 @@ package model
 import (
 	"database/sql"
 	"time"
+
+	"github.com/sandonemaki/my_read_memo_Go_API/backend/pkg/dbmodels"
 )
 
-type User struct {
-	Ulid      string
-	Nickname  string
-	DeletedAt sql.Null[time.Time]
-}
+type User dbmodels.User
 
 func NewUser(
 	ulid string,
