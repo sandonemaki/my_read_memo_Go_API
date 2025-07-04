e5bef1d16fecaef2a91b1cddf3d39c51ee68b7e1
sandonemaki <kamuramaki@gmail.com>
2025-07-04 18:40:19 +0900

    outputの型を追加

--- backend/core/usecase/output/user.go
+++ backend/core/usecase/output/user.go
@@ -7,3 +7,29 @@ import (
 type GetUser struct {
 	User *model.User
 }
+
+func NewGetUser(user *model.User) *GetUser {
+	return &GetUser{
+		User: user,
+	}
+}
+
+type CreateUser struct {
+	User *model.User
+}
+
+func NewCreateUser(user *model.User) *CreateUser {
+	return &CreateUser{
+		User: user,
+	}
+}
+
+type UpdateUser struct {
+	User *model.User
+}
+
+func NewUpdateUser(user *model.User) *UpdateUser {
+	return &UpdateUser{
+		User: user,
+	}
+}
