e00cf8424425c09f5543e3d031eaa0ba61e1e4f0
sandonemaki <kamuramaki@gmail.com>
2025-07-04 18:39:21 +0900

    infra層のget関数をrepo->query

---
+++ backend/core/infra/query/user_impl.go
@@ -0,0 +1,39 @@
+package query
+
+import (
+	"context"
+
+	"github.com/sandonemaki/my_read_memo_Go_API/backend/core/domain/model"
+	"github.com/sandonemaki/my_read_memo_Go_API/backend/core/domain/query"
+	"github.com/sandonemaki/my_read_memo_Go_API/backend/pkg/db"
+	"github.com/sandonemaki/my_read_memo_Go_API/backend/pkg/dbmodels"
+	"github.com/stephenafamo/bob"
+	"github.com/stephenafamo/bob/dialect/psql/dialect"
+)
+
+type user struct {
+	dbClient *db.Client
+}
+
+func NewUser(dbClient *db.Client) query.User {
+	return &user{dbClient}
+}
+
+func (r *user) Get(ctx context.Context, query query.UserGetQuery) (user *model.User, err error) {
+
+	mods := []bob.Mod[*dialect.SelectQuery]{}
+
+	if query.ULID.Valid {
+		mods = append(mods, dbmodels.SelectWhere.Users.Ulid.EQ(query.ULID.String))
+	}
+	if query.UID.Valid {
+		mods = append(mods, dbmodels.SelectWhere.Users.UID.EQ(query.UID.String))
+	}
+
+	dbUser, err := dbmodels.Users.Query(mods...).One(ctx, r.dbClient)
+	if err != nil {
+		return nil, err
+	}
+
+	return (*model.User)(dbUser), nil
+}
