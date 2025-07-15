b345733ebf732b9a091892ddf7130fd89fd6c690
sandonemaki <kamuramaki@gmail.com>
2025-07-15 12:48:20 +0900

    SQLHandler追加

--- backend/pkg/db/db.go
+++ backend/pkg/db/db.go
@@ -5,7 +5,7 @@ import (
 	"database/sql"
 	"time"
 
-	"github.com/stephenafamo/scan"
+	"github.com/sandonemaki/my_read_memo_Go_API/backend/pkg/util"
 )
 
 // Client :
@@ -26,7 +26,19 @@ func NewClient(db *sql.DB) Client {
 	return Client{db}
 }
 
-// SQLHandlerはbobの場合必要ない
+// SQLHandler : BOB版のExecutorインターフェース
+type SQLHandler interface {
+	ExecContext(context.Context, string, ...any) (sql.Result, error)
+	QueryContext(context.Context, string, ...any) (*sql.Rows, error) // scan.Rows → *sql.Rows Z
+}
+
+// Get : コンテキストからトランザクションまたはDBクライアントを取得
+func (c Client) Get(ctx context.Context) SQLHandler {
+	if tx := util.GetDBTx(ctx); tx != nil {
+		return tx // トランザクション内の場合
+	}
+	return c // 通常のDB接続の場合
+}
 
 // ExecContextメソッドは、SQLデータベースに対して更新系のクエリを実行するためのメソッド
 // 主に書き込み系の操作で使用
@@ -36,7 +48,7 @@ func (db Client) ExecContext(ctx context.Context, query string, args ...any) (sq
 
 // QueryContextメソッドは、SQLデータベースに対してSELECT系のクエリを実行するためのメソッド
 // 主に読み取り系の操作で使用
-func (db Client) QueryContext(ctx context.Context, query string, args ...any) (scan.Rows, error) {
+func (db Client) QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error) {
 	rows, err := db.dbClient.QueryContext(ctx, query, args...)
 	if err != nil {
 		return nil, err
