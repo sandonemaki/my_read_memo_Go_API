d68882ce6fc7ec703e8b17cced175a6cdb3a06db
sandonemaki <kamuramaki@gmail.com>
2025-07-28 08:04:26 +0900

    orval作成

--- frontend/orval.config.ts
+++ frontend/orval.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from 'orval';
+import { defineConfig } from "orval";
 
 export default defineConfig({
-  yondeco: {
-    input: {
-      target: '../openapi/openapi.yml',
-    },
-    output: {
-      mode: 'split',
-      target: './app/api/endpoint',
-      schemas: './app/api/models',
-      client: 'swr',
-      mock: true,
-      clean: true,
-    },
-    hooks: {
-      afterAllFilesWrite: "bun run --bun format",
-    },
-  },
+	yondeco: {
+		input: {
+			target: "../openapi/openapi.yml",
+		},
+		output: {
+			mode: "split",
+			target: "./app/api/endpoint",
+			schemas: "./app/api/models",
+			client: "swr",
+			mock: true,
+			clean: true,
+		},
+		hooks: {
+			afterAllFilesWrite: "bun run --bun format",
+		},
+	},
 });
