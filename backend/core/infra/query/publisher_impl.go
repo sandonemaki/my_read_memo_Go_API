5f84b51a80d8f0f763fffbe2af2a6dcea5c78e89
sandonemaki <kamuramaki@gmail.com>
2025-08-05 04:36:14 +0900

    infra/query/publisher_impl.go

---
+++ backend/core/infra/query/publisher_impl.go
@@ -0,0 +1,59 @@
+package query
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+
+	"github.com/sandonemaki/my_read_memo_Go_API/backend/core/domain/model"
+	"github.com/sandonemaki/my_read_memo_Go_API/backend/core/domain/query"
+	"github.com/sandonemaki/my_read_memo_Go_API/backend/pkg/db"
+	"github.com/sandonemaki/my_read_memo_Go_API/backend/pkg/dbmodels"
+	"github.com/stephenafamo/bob"
+	"github.com/stephenafamo/bob/dialect/psql/dialect"
+)
+
+type publisher struct {
+	dbClient *db.Client
+}
+
+func NewPublisher(dbClient *db.Client) *publisher {
+	return &publisher{dbClient}
+}
+
+func (p *publisher) GetPublisherByID(ctx context.Context, query query.PublisherGetQuery, orFail bool) (output *model.Publisher, err error) {
+	mods := []bob.Mod[*dialect.SelectQuery]{}
+
+	if query.ID.Valid {
+		mods = append(mods, dbmodels.SelectWhere.Publishers.ID.EQ(query.ID.Int64))
+	}
+
+	dbPublisher, err := dbmodels.Publishers.Query(mods...).One(ctx, p.dbClient.Get(ctx))
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			if orFail {
+				return nil, err
+			}
+			return nil, nil
+		}
+		return nil, err
+	}
+	return (*model.Publisher)(dbPublisher), nil
+}
+
+func (p *publisher) ListPublishers(ctx context.Context, filter query.PublisherListFilter) (output []*model.Publisher, err error) {
+
+	mods := []bob.Mod[*dialect.SelectQuery]{}
+
+	dbPublishers, err := dbmodels.Publishers.Query(mods...).All(ctx, p.dbClient.Get(ctx))
+	if err != nil {
+		return nil, err
+	}
+	publishers := make([]*model.Publisher, len(dbPublishers))
+	for i, dbPublisher := range dbPublishers {
+		publishers[i] = (*model.Publisher)(dbPublisher)
+	}
+	return publishers, nil
+
+}
