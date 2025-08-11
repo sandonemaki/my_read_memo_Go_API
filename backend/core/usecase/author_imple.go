3d14645b00cc9b2bb9d4ea210715811f36bd01e0
sandonemaki <kamuramaki@gmail.com>
2025-08-11 16:37:21 +0900

    author_impleの修正

--- backend/core/usecase/author_imple.go
+++ backend/core/usecase/author_imple.go
@@ -8,6 +8,7 @@ import (
 	"github.com/sandonemaki/my_read_memo_Go_API/backend/core/domain/repository"
 	"github.com/sandonemaki/my_read_memo_Go_API/backend/core/usecase/input"
 	"github.com/sandonemaki/my_read_memo_Go_API/backend/core/usecase/output"
+	"github.com/volatiletech/null"
 )
 
 type author struct {
@@ -26,16 +27,16 @@ func NewAuthor(
 }
 
 func (a *author) Create(ctx context.Context, in input.CreateAuthor) (*output.CreateAuthor, error) {
-	
+
 	if err := in.Validate(); err != nil {
 		return nil, err
 	}
 
 	author := &model.Author{
-		Name: in.Name
+		Name: in.Name,
 	}
 
-	authorID, err := a.autherRepo.Create(ctx, author)
+	authorID, err := a.authorRepo.Create(ctx, author)
 	if err != nil {
 		return nil, err
 	}
@@ -44,9 +45,25 @@ func (a *author) Create(ctx context.Context, in input.CreateAuthor) (*output.Cre
 }
 
 func (a *author) GetByID(ctx context.Context, authorID int64) (*output.GetAuthor, error) {
-	author, err := a.authorQuery.GetByID(ctx, authorID)
+	author, err := a.authorQuery.GetByID(ctx, query.AuthorGetQuery{
+		ID: null.Int64From(authorID),
+	}, true)
 	if err != nil {
 		return nil, err
 	}
 	return output.NewGetAuthor(author), nil
+}
+
+func (a *author) List(ctx context.Context) (*output.ListAuthors, error) {
+	authors, err := a.authorQuery.List(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return output.NewListAuthors(authors), nil
+}
+
+func (a *author) SearchByName(ctx context.Context, name string) (*output.ListAuthors, error) {
+
+	return output.NewListAuthors(([]*model.Author{})), nil
 }
