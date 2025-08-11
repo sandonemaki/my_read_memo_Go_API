c0cce7430bc7149cd1e938033e8f54c3cf61b48c
sandonemaki <kamuramaki@gmail.com>
2025-08-11 16:38:13 +0900

    実装がなくても500エラーを返すようにunimplemented.goを作成

--- backend/core/handler/unimplemented.go
+++ backend/core/handler/unimplemented.go
@@ -32,3 +32,46 @@ func (_ Unimplemented) UpdateMe(ctx context.Context, request oapi.UpdateMeReques
 		},
 	}, nil
 }
+
+func (_ Unimplemented) CreateAuthor(ctx context.Context, request oapi.CreateAuthorRequestObject) (oapi.CreateAuthorResponseObject, error) {
+	return oapi.CreateAuthor500JSONResponse{
+		InternalServerErrorJSONResponse: oapi.InternalServerErrorJSONResponse{
+			Message: "unimplemented",
+		},
+	}, nil
+}
+func (_ Unimplemented) GetAuthorById(ctx context.Context, request oapi.GetAuthorByIdRequestObject) (oapi.GetAuthorByIdResponseObject, error) {
+	return oapi.GetAuthorById500JSONResponse{
+		InternalServerErrorJSONResponse: oapi.InternalServerErrorJSONResponse{
+			Message: "unimplemented",
+		},
+	}, nil
+}
+func (_ Unimplemented) GetAuthors(ctx context.Context, request oapi.ListAuthorsRequestObject) (oapi.ListAuthorsResponseObject, error) {
+	return oapi.GetAuthors500JSONResponse{
+		InternalServerErrorJSONResponse: oapi.InternalServerErrorJSONResponse{
+			Message: "unimplemented",
+		},
+	}, nil
+}
+func (_ Unimplemented) CreatePublisher(ctx context.Context, request oapi.CreatePublisherRequestObject) (oapi.CreatePublisherResponseObject, error) {
+	return oapi.CreatePublisher500JSONResponse{
+		InternalServerErrorJSONResponse: oapi.InternalServerErrorJSONResponse{
+			Message: "unimplemented",
+		},
+	}, nil
+}
+func (_ Unimplemented) GetPublisherById(ctx context.Context, request oapi.GetPublisherByIdRequestObject) (oapi.GetPublisherByIdResponseObject, error) {
+	return oapi.GetPublisherById500JSONResponse{
+		InternalServerErrorJSONResponse: oapi.InternalServerErrorJSONResponse{
+			Message: "unimplemented",
+		},
+	}, nil
+}
+func (_ Unimplemented) GetPublishers(ctx context.Context, request oapi.ListPublishersRequestObject) (oapi.ListPublishersResponseObject, error) {
+	return oapi.GetPublishers500JSONResponse{
+		InternalServerErrorJSONResponse: oapi.InternalServerErrorJSONResponse{
+			Message: "unimplemented",
+		},
+	}, nil
+}
