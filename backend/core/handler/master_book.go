562b9a5cf4b81d5a75f10e60acca733efd930e3d
sandonemaki <kamuramaki@gmail.com>
2025-08-27 07:27:58 +0900

    handler層の使用しない引数をアンスコに変更

--- backend/core/handler/master_book.go
+++ backend/core/handler/master_book.go
@@ -6,27 +6,27 @@ import (
 	"github.com/sandonemaki/my_read_memo_Go_API/backend/pkg/oapi"
 )
 
-func (h Core) ListMasterBooks(ctx context.Context, request oapi.ListMasterBooksRequestObject) (oapi.ListMasterBooksResponseObject, error) {
+func (h Core) ListMasterBooks(ctx context.Context, _ oapi.ListMasterBooksRequestObject) (oapi.ListMasterBooksResponseObject, error) {
 	// TODO: 実装
-	return oapi.ListMasterBooks200JSONResponse{}, nil
+	return oapi.ListMasterBooks200JSONResponse{Books: []oapi.MasterBook{}}, nil
 }
 
-func (h Core) CreateMasterBook(ctx context.Context, request oapi.CreateMasterBookRequestObject) (oapi.CreateMasterBookResponseObject, error) {
+func (h Core) CreateMasterBook(ctx context.Context, _ oapi.CreateMasterBookRequestObject) (oapi.CreateMasterBookResponseObject, error) {
 	// TODO: 実装
 	return oapi.CreateMasterBook201JSONResponse{}, nil
 }
 
 func (h Core) SearchMasterBooks(ctx context.Context, request oapi.SearchMasterBooksRequestObject) (oapi.SearchMasterBooksResponseObject, error) {
 	// TODO: 実装
-	return oapi.SearchMasterBooks200JSONResponse{}, nil
+	return oapi.SearchMasterBooks200JSONResponse{Books: []oapi.MasterBook{}}, nil
 }
 
-func (h Core) GetMasterBookById(ctx context.Context, request oapi.GetMasterBookByIdRequestObject) (oapi.GetMasterBookByIdResponseObject, error) {
+func (h Core) GetMasterBookById(ctx context.Context, _ oapi.GetMasterBookByIdRequestObject) (oapi.GetMasterBookByIdResponseObject, error) {
 	// TODO: 実装
 	return oapi.GetMasterBookById200JSONResponse{}, nil
 }
 
-func (h Core) UpdateMasterBook(ctx context.Context, request oapi.UpdateMasterBookRequestObject) (oapi.UpdateMasterBookResponseObject, error) {
+func (h Core) UpdateMasterBook(ctx context.Context, _ oapi.UpdateMasterBookRequestObject) (oapi.UpdateMasterBookResponseObject, error) {
 	// TODO: 実装
 	return oapi.UpdateMasterBook200JSONResponse{}, nil
 }
